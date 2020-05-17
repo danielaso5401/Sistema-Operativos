3e39a42c188c6d9a950e33a965c0a8fc18043a7d
danielaso5401 <42683702+danielaso5401@users.noreply.github.com>
2020-05-17 14:44:39 -0500

    Update main_ciclico.java

--- laboratorio_SO/filosofos_ciclicos/main_ciclico.java
+++ laboratorio_SO/filosofos_ciclicos/main_ciclico.java
@@ -1,16 +1,17 @@
-package so4;
+package filosofos ciclicos;
 
 public class Simulacion01 {
 
 	public static void main(String[] args)throws InterruptedException {
 		
-		Filosofo[] fil=new Filosofo[5];
+		Filosofo[] fil=new Filosofo[5];//creamos el tamaño deseado de filsoofos
 		int i=0;
 		while(true) {
 			fil[i]=new Filosofo(i);
-			
 			fil[i].start();
 			fil[i].join();
+			/*usamos el metodo joing de manera que una vez que un filosofo piense pase al siguiente y de esa manera
+			tenerlo controlado*/
 			i++;
 			if(i==5){
 				i=0;
